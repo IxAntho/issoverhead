5f34864bb6dba116d52cd58dae4228ffe62776e1
IxAntho <isabar735@icloud.com>
2024-02-08 20:09:53 -0500

    Complete project

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import requests
 from datetime import datetime
+from smtplib import SMTP
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 51.507351  # Your latitude
+MY_LONG = -0.127758  # Your longitude
+MARGIN = 5
+MY_EMAIL = "ixdul3004@gmail.com"
+MY_PASSWORD = "rbpc prwc wmvj yryw"
+OTHER_EMAIL = "ixdul3004@gmail.com"
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -11,7 +16,7 @@ data = response.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
-#Your position is within +5 or -5 degrees of the ISS position.
+# Your position is within +5 or -5 degrees of the ISS position.
 
 
 parameters = {
@@ -27,11 +32,18 @@ sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
+time_hour = time_now.hour
 
-#If the ISS is close to my current position
+# If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
 
-
-
+if sunset <= time_hour < sunrise:
+    if MY_LAT - MARGIN <= iss_latitude <= MY_LAT + MARGIN:
+        with SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=OTHER_EMAIL,
+                                msg=f"Subject: Hey look up at the sky\n\nLook up")
+            print("Email sent")
